14da7934a16787bb894fa318b5699384be3a57a7
Sharky_BSA <sharky.tf@gmail.com>
2026-01-08 22:37:08 +0100

    003 Arme full auto MODIF: Changement de l'aspect : rayon a une largeur (un rayon haha)

--- weapons/player_weapons/full_auto/weapon_full_auto.gd
+++ weapons/player_weapons/full_auto/weapon_full_auto.gd
@@ -3,9 +3,12 @@
 class_name Weapon_FullAuto extends Weapon
 
 @export_range(1.0,60,0.1) var shots_per_secs : float = 20
+@export var beam_width : float = 30
 
 @onready var particles_left: GPUParticles2D = %ParticlesLeft
 @onready var particles_right: GPUParticles2D = %ParticlesRight
+@onready var bullet_spawn_point: Marker2D = %BulletSpawnPoint
+
 var particle_left_material : ParticleProcessMaterial 
 var particle_right_material : ParticleProcessMaterial 
 
@@ -38,7 +41,8 @@ func _physics_process(_delta: float) -> void:
 func shoot()->void:
 	var bullet : Bullet = bullet_scene.instantiate()
 	bullet.global_rotation = global_rotation +randf_range(-spread/2,spread/2)
-	bullet.global_position = global_position+Vector2(100,0).rotated(global_rotation)
+	var spawn_position_offset = Vector2(0,randf_range(-beam_width,+beam_width)).rotated(global_rotation)
+	bullet.global_position = bullet_spawn_point.global_position + spawn_position_offset
 	bullet.max_range = max_range
 	bullet.speed = max_speed
 	bullet.scale = Vector2.ONE *0.5
