a940cbdfd7c21e97c492d8dc9f2b76975a021bae
Sharky_BSA <sharky.tf@gmail.com>
2026-01-08 21:58:00 +0100

    001 Level graph auto_complete Un level item est ajouté automatiquement lorsqu'on ajoute un nouvel élément à LevelGraph

--- levels/levels/level_graph.gd
+++ levels/levels/level_graph.gd
@@ -1,7 +1,12 @@
 @tool
 class_name Level_Graph extends Resource
 
-@export var level_list : Array[Level_Item]
+@export var level_list : Array[Level_Item]:
+	set(val):
+		level_list=val
+		for i in range(level_list.size()):
+			if level_list[i]==null:
+				level_list[i]=Level_Item.new()
 
 var _current_level_index : int = 0
 
